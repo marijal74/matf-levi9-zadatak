242566ebb01743d6b6db8cf4df8da135942da55f
marijal74 <lakic.marija98@gmail.com>
2020-12-21 20:54:04 +0100

    [feat/input_item] Implement handles
    Implemented handles for input of name and desc. Changed class name to
    AddShopItem.

--- matf-levi9-zadatak/src/components/App/App.js
+++ matf-levi9-zadatak/src/components/App/App.js
@@ -1,25 +1,13 @@
-import logo from '../../logo.svg';
 import './App.css';
-import ShopItem from '../ShopItem/ShopItem';
+import ShopItem from '../ShopItem/CreateShopItem';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <ShopItem></ShopItem>  
       </header>
-      <ShopItem></ShopItem>
+      
     </div>
     
   );
